2232bd04f6038d702061811e2de02ae0bece53fd
Torsten Petersdorf <tp@tpete.de>
2015-06-16 10:56:43 +0200

    Change: Use semantic configuration

--- DependencyInjection/CheckdomainTeleCashExtension.php
+++ DependencyInjection/CheckdomainTeleCashExtension.php
@@ -18,6 +18,21 @@ class CheckdomainTeleCashExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config        = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('checkdomain_tele_cash.service_url', $config['service_url']);
+
+        $container->setParameter('checkdomain_tele_cash.api.user', $config['api']['user']);
+        $container->setParameter('checkdomain_tele_cash.api.pass', $config['api']['pass']);
+
+        $container->setParameter('checkdomain_tele_cash.client_cert_path', $config['client_cert_path']);
+
+        $container->setParameter('checkdomain_tele_cash.client_key.path', $config['client_key']['path']);
+        $container->setParameter('checkdomain_tele_cash.client_key.pass_phrase', $config['client_key']['pass_phrase']);
+
+        $container->setParameter('checkdomain_tele_cash.server_cert_path', $config['server_cert_path']);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,6 +16,26 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $rootNode    = $treeBuilder->root('checkdomain_tele_cash');
+
+        $rootNode
+            ->children()
+                ->arrayNode('api')
+                    ->children()
+                        ->scalarNode('user')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('pass')->isRequired()->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+                ->arrayNode('client_key')
+                    ->children()
+                        ->scalarNode('path')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('pass_phrase')->isRequired()->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+                ->scalarNode('client_cert_path')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('server_cert_path')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('service_url')->isRequired()->cannotBeEmpty()->end()
+            ->end();
 
         return $treeBuilder;
     }
